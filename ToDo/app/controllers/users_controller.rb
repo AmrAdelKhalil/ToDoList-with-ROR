3884bf155f5667ea7117ec1e7ade98a31976c1ab
AmrAdelKhalil <amradelkhalil@gmail.com>
2016-02-09 19:02:30 +0200

    Login plus Logout plus show in user controller

--- ToDo/app/controllers/users_controller.rb
+++ ToDo/app/controllers/users_controller.rb
@@ -1,16 +1,27 @@
 class UsersController < ApplicationController
-  layout false
-  def index
-  end
+   layout false
+    
+    def login
+    end
+    
+   def index
+   end
 
-  def show
-  end
+   def show
+       @user = User.find_by_email(params[:user][:email])
+       
+       if @user && @user.password ==params[:user][:password]
+#           doNoThing
+       else
+           redirect_to(:action => 'login')
+       end
+   end
 
-  def new
+   def new
 #      @user=User.new({:first_name => "Ex.Amr",:second_name => "Ex.Adel", :email => "blabla@bla.com"})
-  end
-
-    def create
+   end
+    
+   def create
         @user = User.new(user_pram)
         
         if @user.save
@@ -18,16 +29,16 @@ class UsersController < ApplicationController
         else
             render('new')
         end
-    end
+   end
 
-  def edit
-  end
+   def edit
+   end
 
-  def delete
-  end
+   def delete
+   end
     
-    private 
-    def user_pram
-        params.require(:user).permit(:first_name,:second_name,:email,:password)
-    end
+   private 
+   def user_pram
+      params.require(:user).permit(:first_name,:second_name,:email,:password)
+   end
 end
